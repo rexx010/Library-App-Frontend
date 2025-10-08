80a6ac8029883b22bbd85ce6050df79064b9426a
rexx010 <grexx200@gmail.com>
2025-10-08 14:36:11 +0100

    Added User page with book borrowing UI
    Introduces User.jsx component displaying a list of books with images, titles, authors, availability, and a borrow button. This serves as the user interface for borrowing books in the library app.

---
+++ library-app/src/pages/users/User.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+       
+        <div className="usermain">
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1629992101753-56d196c8aabb?q=80&w=690&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1589829085413-56de8ae18c73?q=80&w=1212&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1621351183012-e2f9972dd9bf?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1511108690759-009324a90311?q=80&w=688&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1621827979802-6d778e161b28?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1716892001560-935f123683f4?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1639690283395-b62444cf9a76?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1610882648335-ced8fc8fa6b6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+            <div className='books'>
+                <img src="https://images.unsplash.com/photo-1722706731906-2163a06f3022?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fGJvb2slMjBjb3ZlcnxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+                <h4>Pride and Prejudice</h4>
+                <p>Jane Austen</p>
+                <h5>4/4 available</h5>
+                <button>Borrow</button>
+            </div>
+           
+        </div>
+    </div>
+  )
+}
+
+export default User
